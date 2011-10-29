1f61bf0e923d46caccef0766f3fa96ef2e968179
wizard23 <wizard23@PHOENIX.local>
2011-10-29 17:26:02 +0200

    clicking on one of the CAs

--- src/helpers.js
+++ src/helpers.js
@@ -117,3 +117,24 @@ function getGL(c) {
 	}
 	return gl;
 }
+
+
+function relMouseCoords(event){
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = this;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft;
+        totalOffsetY += currentElement.offsetTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+
+    return {x:canvasX, y:canvasY}
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
