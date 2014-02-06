b888d6e69777b72db1e510b0fd0a8e110df4d082
Philipp Tiefenbacher <wizards23+bitbucket@gmail.com>
2014-02-06 19:46:13 +0100

    drafting modules

--- src/game/CellSpace.js
+++ src/game/CellSpace.js
@@ -1,6 +1,24 @@
-var EvoCell;
-if (!EvoCell) {
-    EvoCell = {};
+require.config({
+    baseUrl: 'js/lib',
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        jquery: 'jquery-1.9.0'
+    }
+});
+
+require(["jquery", "EvoCell", "CellSpaceResources"], 
+	function($, EvoCell, resources) {
+		// Setup core and rules and texture
+		var context = document.getElementById('c');
+		var reactor = new  EvoCell.Reactor(context)
+	}
 }
 
 // RulesTexture and Dishes brauchen gemeinsames Basisobjekt das eine Textur zurueckgibt, RuleTexture ist auch ein shader
