83407e5bb1b13c212bd796934b72ef7d974fad1a
wizard23 <wizards23+git@gmail.com>
2014-04-30 01:40:21 +0200

    hacking on db code, cleanup

--- src/data/FileStore.js
+++ src/data/FileStore.js
@@ -1,316 +1,148 @@
 define(["underscore", "backbone"], function(_, bb) {
+  var indexedDB = window.indexedDB;
 
-var indexedDB = window.indexedDB;
+  const dbName = "EvoCell";
+  var db = null;
 
-const dbName = "EvoCell";
-var db = null;
+  const ruleStoreName = "rules";
+  var ruleStore = null;
 
-const ruleStoreName = "rules";
-var ruleStore = null;
+  var readyFns = [];
 
-var readyFns = [];
-
-var ready = function(fn) {
-  if (db) fn();
-  else {
-    readyFns.push(fn);
-  }
-};
-
-var request = indexedDB.open(dbName, 2);
-request.onerror = function(event) {
-  alert("Oh no! Why didn't you allow my web app to use IndexedDB?! Cell is sad now!");
-};
-request.onsuccess = function(event) {
-  db = event.target.result;
-
-  _.each(readyFns, function(fn) { fn(); });
-
-  db.onerror = function(event) {
-    // Generic error handler for all errors targeted at this database's requests!
-    alert("Database error: " + event.target.errorCode);
-  };
-
-  // var objectStore = db.transaction(ruleStoreName).objectStore(ruleStoreName);
-  // objectStore.openCursor().onsuccess = function(event) {
-  //   var cursor = event.target.result;
-  //   if (cursor) {
-  //     //alert("Rule " + cursor.value.id + " is " + cursor.value.ruleTableSize);
-  //     cursor.continue();
-  //   }
-  //   else {
-  //     //alert("No more rules!");
-  //   }
-  // };
-
-};
-
-request.onupgradeneeded = function(event) { 
-  //alert("upgrade from: " + event.oldVersion + " to: " + event.newVersion);
-  var db = event.target.result;
-
-  if(db.objectStoreNames.contains(ruleStoreName)) {
-    db.deleteObjectStore(ruleStoreName);
-  }
-
-  var objectStore = db.createObjectStore(ruleStoreName, { keyPath: "name"});
-  var objectStore = db.createObjectStore("gameStates", { keyPath: "name"});
-
-  // Create an index to search customers by name. We may have duplicates
-  // so we can't use a unique index.
-  //objectStore.createIndex("name", "name", { unique: true });
-};
-
-var addObject = function(storeName, data, callback) {
-  var transaction = db.transaction([storeName], "readwrite");
-  transaction.oncomplete = function(e) { 
-    if (callback) callback(); 
-  };
-  var objectStore = transaction.objectStore(storeName);
-  var request = objectStore.add(data);
-  request.onerror = function(event) {
-    alert("me so sorry, could not add: " + data + "\n" + storeName);
-  };
-  request.onsuccess = function(event) {
-    // dont call callback here call it later when transaction is done!
-  };
-};
-
-var deleteObject = function(storeName, id, callback) {
-  var transaction = db.transaction([storeName], "readwrite");
-  transaction.oncomplete = function(e) { 
-    if (callback) callback(); 
-  };
-  var request = transaction
-                .objectStore(storeName)
-                .delete(id);
-  request.onerror = function(event) {
-    alert("me so sorry, could not delte object id:" + id);
-  };
-  request.onsuccess = function(event) {
-    // It's gone!
-  };
-};
-var getObject = function(storeName, id, callback) {
-  var objectStore = db.transaction([storeName]).objectStore(storeName);
-  var request = objectStore.get(id);
-  request.onerror = function(event) {
-    alert("me so sorry, could not load object with id name:" + id);
-  };
-  request.onsuccess = function(event) {
-    var result = event.target.result;
-    if (!result)
-      alert("Empty result for id: " + id);
-    callback(result);
-  };
-};
-var getAllObjects = function(storeName, callback, filter) {
-  filter = filter || function(x) { return true; };
-  var  objects = [];
-  var objectStore = db.transaction(ruleStoreName).objectStore(storeName);
-
-  objectStore.openCursor().onsuccess = function(event) {
-    var cursor = event.target.result;
-    if (cursor) {
-      if (filter(cursor.value))
-        objects.push(cursor.value);
-      cursor.continue();
-    }
+  var ready = function(fn) {
+    if (db) fn();
     else {
-      callback(objects);
+      readyFns.push(fn);
     }
   };
-};
-var getAllObjectIds = function(storeName, callback, filter) {
-  getAllObjects(function(rulesModelData) {
-    var ids = _.map(rulesModelData, function(obj) { return obj.id; });
-    callback(ids);
-  }, filter);
-} ;
 
-
-
-var storeRule = function(name, ruleData, callback) {
-  var transaction = db.transaction([ruleStoreName], "readwrite");
-   transaction.oncomplete = function(e) { 
-    if (callback) callback(); 
-  };
-
-  var objectStore = transaction.objectStore(ruleStoreName);
-  var request = objectStore.add({name:name, ruleData:ruleData});
+  var request = indexedDB.open(dbName, 2);
   request.onerror = function(event) {
-    alert("me so sorry, could not save: " + name + "\n" + ruleData);
+    alert("Oh no! Why didn't you allow my web app to use IndexedDB?! Cell is sad now!");
   };
   request.onsuccess = function(event) {
-    // dont call callback here call it later when transaction is done!
-    //if (callback) callback();
-  };
-};
+    db = event.target.result;
 
-var deleteRule = function(name, callback) {
-  var transaction = db.transaction([ruleStoreName], "readwrite");
-  //transaction.onabort = function(e) { alert("atransbort"); };
-  transaction.oncomplete = function(e) { 
-   if (callback) callback(); 
-  };
-  
-  var request = transaction
-                .objectStore(ruleStoreName)
-                .delete(name);
-  request.onerror = function(event) {
-    alert("me so sorry, could not delte rule named:" + name);
-  };
-  request.onsuccess = function(event) {
-    // It's gone!
-  };
-};
+    _.each(readyFns, function(fn) { fn(); });
 
-var loadRule = function(name, callback) {
-  var objectStore = db.transaction([ruleStoreName]).objectStore(ruleStoreName);
-  var request = objectStore.get(name);
-  request.onerror = function(event) {
-    alert("me so sorry, could not load rule with id name:" + name);
-  };
-  request.onsuccess = function(event) {
-    var result = event.target.result;
-    if (!result)
-      alert("Empty result for rule: " + name);
-    callback(result);
-  };
-};
-
-var loadAllRules = function(callback) {
-  var rules = [];
-  var objectStore = db.transaction(ruleStoreName).objectStore(ruleStoreName);
-  objectStore.openCursor().onsuccess = function(event) {
-    var cursor = event.target.result;
-    if (cursor) {
-      //alert("Rule " + cursor.value.id + " is " + cursor.value.ruleTableSize);
-      rules.push(cursor.value);
-      cursor.continue();
-    }
-    else {
-      callback(rules);
-    }
-  };
-};
-
-var loadAllRuleNames = function(callback) {
-  loadAllRules(function(rulesModelData) {
-    var names = [];
-    for (var ruleIdx in rulesModelData) {
-      var rule = rulesModelData[ruleIdx];
-      //if (rule.ruleData.contaisNeighbourhood)
-      names.push(rule.name);
-    }
-    callback(names);
-  });
-} ;
-
-
-return {
-  storeRule: storeRule,
-  deleteRule: deleteRule,
-  loadRule: loadRule,
-  loadAllRules: loadAllRules,
-  loadAllRuleNames: loadAllRuleNames,
-  ready: ready,
-};
-
-
-
-
-
-  /*var objectStore = db.transaction(["customers"], "readwrite").objectStore("customers");
-  var request = objectStore.get("444-44-4444");
-  request.onerror = function(event) {
-    // Handle errors!
-  };
-  request.onsuccess = function(event) {
-    // Get the old value that we want to update
-    var data = request.result;
-    
-    // update the value(s) in the object that you want to change
-    data.age = 42;
-
-    // Put this updated object back into the database.
-    var requestUpdate = objectStore.put(data);
-     requestUpdate.onerror = function(event) {
-       // Do something with the error
-        alert("why u no update?");
-     };
-     requestUpdate.onsuccess = function(event) {
-       // Success - the data is updated!
-      alert("I haz updated");
-     };
-  };
-
-
-var index = objectStore.index("name");
-  // Using a normal cursor to grab whole customer record objects
-index.openCursor().onsuccess = function(event) {
-  var cursor = event.target.result;
-  if (cursor) {
-    // cursor.key is a name, like "Bill", and cursor.value is the whole object.
-    alert("Name: " + cursor.key + ", SSN: " + cursor.value.ssn + ", email: " + cursor.value.email);
-    cursor.continue();
-  }
-};
-return;
-// Using a key cursor to grab customer record object keys
-index.openKeyCursor().onsuccess = function(event) {
-  var cursor = event.target.result;
-  if (cursor) {
-    // cursor.key is a name, like "Bill", and cursor.value is the SSN.
-    // No way to directly get the rest of the stored object.
-    alert("Name: " + cursor.key + ", SSN: " + cursor.value);
-    cursor.continue();
-  }
-};
-
-/*
-  var objectStore2 = db.transaction("customers").objectStore("customers");
-
-  objectStore2.openCursor().onsuccess = function(event) {
-    var cursor = event.target.result;
-    if (cursor) {
-      alert("Name for SSN " + cursor.key + " is " + cursor.value.name);
-      cursor.continue();
-    }
-    else {
-      alert("No more entries!");
-    }
-  };
-*/
-/*
-// readonly
-  var transaction = db.transaction(["customers"]);
-  var objectStore = transaction.objectStore("customers");
-  var request = objectStore.get("444-44-4444");
-  request.onerror = function(event) {
-    // Handle errors!
-  };
-  request.onsuccess = function(event) {
-    // Do something with the request.result!
-    alert("Name for SSN 444-44-4444 is " + request.result.name);
-  };
-*/
-
-/*
-  // readwrite insert
-  var transaction = db.transaction(["customers"], "readwrite");
-  // Do something when all the data is added to the database.
-  transaction.oncomplete = function(event) {
-    alert("All done!");
-  };
-
-  var objectStore = transaction.objectStore("customers");
-  for (var i in customerData) {
-    var request = objectStore.add(customerData[i]);
-    request.onsuccess = function(event) {
-      // event.target.result == customerData[i].ssn;
-      alert("added" + event.target.result)
+    db.onerror = function(event) {
+      // Generic error handler for all errors targeted at this database's requests!
+      alert("Database error: " + event.target.errorCode);
     };
-  }
-  */
+  };
+
+  request.onupgradeneeded = function(event) { 
+    //alert("upgrade from: " + event.oldVersion + " to: " + event.newVersion);
+    var db = event.target.result;
+
+    if(db.objectStoreNames.contains(ruleStoreName)) {
+      db.deleteObjectStore(ruleStoreName);
+    }
+
+    var objectStore = db.createObjectStore(ruleStoreName, { keyPath: "name"});
+    var objectStore = db.createObjectStore("gameStates", { keyPath: "name"});
+
+    // Create an index to search customers by name. We may have duplicates
+    // so we can't use a unique index.
+    //objectStore.createIndex("name", "name", { unique: true });
+  };
+
+  var addObject = function(storeName, data, callback) {
+    var transaction = db.transaction([storeName], "readwrite");
+    transaction.oncomplete = function(e) { 
+      if (callback) callback(); 
+    };
+    var objectStore = transaction.objectStore(storeName);
+    var request = objectStore.add(data);
+    request.onerror = function(event) {
+      alert("me so sorry, could not add: " + data + "\n" + storeName);
+    };
+    request.onsuccess = function(event) {
+      // dont call callback here call it later when transaction is done!
+    };
+  };
+
+  var deleteObject = function(storeName, id, callback) {
+    var transaction = db.transaction([storeName], "readwrite");
+    transaction.oncomplete = function(e) { 
+      if (callback) callback(); 
+    };
+    var request = transaction
+                  .objectStore(storeName)
+                  .delete(id);
+    request.onerror = function(event) {
+      alert("me so sorry, could not delte object id:" + id);
+    };
+    request.onsuccess = function(event) {
+      // It's gone!
+    };
+  };
+  var getObject = function(storeName, id, callback) {
+    var objectStore = db.transaction([storeName]).objectStore(storeName);
+    var request = objectStore.get(id);
+    request.onerror = function(event) {
+      alert("me so sorry, could not load object with id name:" + id);
+    };
+    request.onsuccess = function(event) {
+      var result = event.target.result;
+      if (!result)
+        alert("Empty result for id: " + id);
+      callback(result);
+    };
+  };
+  var getAllObjects = function(storeName, callback, filter) {
+    filter = filter || function(x) { return true; };
+    var  objects = [];
+    var objectStore = db.transaction(ruleStoreName).objectStore(storeName);
+
+    objectStore.openCursor().onsuccess = function(event) {
+      var cursor = event.target.result;
+      if (cursor) {
+        if (filter(cursor.value))
+          objects.push(cursor.value);
+        cursor.continue();
+      }
+      else {
+        callback(objects);
+      }
+    };
+  };
+
+  var storeRule = function(name, ruleData, callback) {
+    addObject(ruleStoreName, {name: name, ruleData: ruleData}, callback);
+  };
+
+  var deleteRule = function(name, callback) {
+    deleteObject(ruleStoreName, name, callback);
+  };
+
+  var loadRule = function(name, callback) {
+    getObject(ruleStoreName, name, callback);
+  };
+
+  var loadAllRules = function(callback) {
+    getAllObjects(ruleStoreName, callback);
+  };
+
+  var loadAllRuleNames = function(callback) {
+    getAllObjects(ruleStoreName, function(rules) {
+      callback(_.map(rules, function(rule) { return rule.name; }));
+    });
+  } ;
+
+  return {
+    addObject: addObject,
+    deleteObject: deleteObject,
+    getObject: getObject,
+    getAllObjects: getAllObjects,
+
+    // depricated "rule" functions use addObject, deleteObject, ... insted
+    storeRule: storeRule,
+    deleteRule: deleteRule,
+    loadRule: loadRule,
+    loadAllRules: loadAllRules,
+    loadAllRuleNames: loadAllRuleNames,
+
+    ready: ready,
+  };
 });
