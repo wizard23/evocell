a8a945496d4b4f99eeabd47355354920468862c2
wizard23 <wizards23@gmail.com>
2014-05-13 06:29:50 +0200

    added (non "compiling" tests, TODO: move all comments from stable to tests

---
+++ src/Tests.js
@@ -0,0 +1,40 @@
+
+		// document.getElementById("saveGameState").addEventListener("click", function(evt) {
+			
+		// 			fileStore.addObject("gameStates", {id: "test23XXXX", state: {txt: "txt", nr: 123} },
+		// 				function() { alert("works" + 23); },
+		// 				function() { alert("NOT: " + 23); }
+		// 			);
+			
+		// }, false);
+
+		// var works = ["yo", 12];
+		// var notworks = ["yo", 12];
+
+		// document.getElementById("saveGameState").addEventListener("click", function(evt) {
+		// 	works = [];
+		// 	notworks = [];
+		// 	_.each(gameState, function(value, key) { 
+		// 		var clonedState = {};
+		// 		clonedState[key] = value;
+
+		// 		try {
+		// 			fileStore.addObject("gameStates", {id: "test23" + key, state: clonedState },
+		// 				function() { 
+		// 				//	alert("works" + key); 
+		// 				},
+		// 				function() { 
+		// 				// alert("NOT: " + key); 
+		// 				}
+		// 			);
+
+		// 			works.push("\"" + key + "\"");
+		// 		}
+		// 		catch (ex) {
+		// 			//alert("Except: " + key);
+		// 			notworks.push("\"" + key + "\"");
+		// 		}
+
+		// 	});		
+		// 	alert(notworks);
+		// }, false);
