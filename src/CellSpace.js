65eaedd29d8f4f071ab1d735d6d9590d9f2cd712
7yl4r <murray.tylar@gmail.com>
2015-01-27 00:42:54 -0500

    now with cdn-hosting!

--- src/CellSpace.js
+++ src/CellSpace.js
@@ -3,17 +3,17 @@ var perfStartedJSTime = performance.now();
 require.config({
 	baseUrl: 'src',
 	paths: {
-		jquery: '../../res_evocell/src/libs/jquery-1.10.2',
+		jquery: 'https://code.jquery.com/jquery-1.11.2.min',
 		"jquery-ui": '../../res_evocell/src/libs/jquery-ui-1.10.4.custom',
-		"underscore": "../../res_evocell/src/libs/underscore",
-		backbone: "../../res_evocell/src/libs/backbone",
-		knockback: "../../res_evocell/src/libs/knockback",
-		knockout: "../../res_evocell/src/libs/knockout-3.0.0",
+		"underscore": "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min",
+		backbone: "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min",
+		knockback: "//cdnjs.cloudflare.com/ajax/libs/knockback/0.20.5/knockback.min",
+		knockout: "//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-min",
 		meSpeak: "../../res_evocell/src/libs/mespeak/mespeak",
-		three: "../../res_evocell/src/libs/three", 
-		"jquery-cycle": "../../res_evocell/src/libs/jquery.cycle.all",
-		datgui: "../../res_evocell/src/libs/dat.gui.min",
-		FileSaver: "../../res_evocell/src/libs/FileSaver",
+		three: "//cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min",
+		"jquery-cycle": "//cdnjs.cloudflare.com/ajax/libs/jquery.cycle/3.03/jquery.cycle.all.min",
+		datgui: "//cdnjs.cloudflare.com/ajax/libs/dat-gui/0.5/dat.gui.min",
+		FileSaver: "//cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2014-11-29/FileSaver.min",
 	},
 	shim: {
 		datgui: {
