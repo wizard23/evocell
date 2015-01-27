fbb73b51ec34c8c9655f9c3ff0a29683c3e751b4
7yl4r <murray.tylar@gmail.com>
2015-01-27 01:13:04 -0500

    formatting nazi is at it again!

--- src/CellSpace.js
+++ src/CellSpace.js
@@ -50,42 +50,58 @@ require.config({
 });
 
 
-require([
-	"jquery-ui", "Utils", "EvoCell", "story/StoryTeller", "underscore", 
-	"backbone", "knockback", "knockout", "data/FileStore", "three", "datgui", 
-	"CellSpace/State", "CellSpace/Setup", "CellSpace/GameLoop", "CellSpace/GUI", "CellSpace/Utils"], 
-function($, utils, EC, storyTeller,_ , Backbone, kb, ko, fileStore, THREE, dat, 
-	gameState, csSetup, csGame, csUI, csUtils) {
-	"use strict";
+require(
+	[
+		"jquery-ui",
+		"Utils",
+		"EvoCell",
+		"story/StoryTeller",
+		"underscore",
+		"backbone",
+		"knockback",
+		"knockout",
+		"data/FileStore",
+		"three",
+		"datgui",
+		"CellSpace/State",
+		"CellSpace/Setup",
+		"CellSpace/GameLoop",
+		"CellSpace/GUI",
+		"CellSpace/Utils"
+	],
+	function($, utils, EC, storyTeller,_ , Backbone, kb, ko, fileStore, THREE, dat,
+        gameState, csSetup, csGame, csUI, csUtils) {
+        "use strict";
 
-	gameState.perfRequireTime = performance.now();
-	gameState.perfStartedJSTime = perfStartedJSTime;
+        gameState.perfRequireTime = performance.now();
+        gameState.perfStartedJSTime = perfStartedJSTime;
 
-	// MAIN LOOP (must be less than 20 LOC :)
+        // MAIN LOOP (must be less than 20 LOC :)
 
-	// TODO: extract this in CellSpace.App which bundles State, Setup, UI, ...
+        // TODO: extract this in CellSpace.App which bundles State, Setup, UI, ...
 
-	// TODO: should we not put this in backbone ready function?
-	// is jquery ready better?
-	//$(window).load(function(e) { 
-		var canvas = document.getElementById('c');
+        // TODO: should we not put this in backbone ready function?
+        // is jquery ready better?
+        //$(window).load(function(e) {
+            var canvas = document.getElementById('c');
 
-		csSetup.setup(canvas, function () {
-			csUI.setupGui();
+            csSetup.setup(canvas, function () {
+                csUI.setupGui();
 
-			gameState.mainLoop = new utils.AnimationLoop(0, function() {
-				csUI.pollUserInteraction();
-				if (!gameState.pause || gameState.doOneStep) {
-					csGame.step();
-					gameState.doOneStep = false;
-				}
-				csGame.render();
-			});
+                gameState.mainLoop = new utils.AnimationLoop(0, function() {
+                    csUI.pollUserInteraction();
+                    if (!gameState.pause || gameState.doOneStep) {
+                        csGame.step();
+                        gameState.doOneStep = false;
+                    }
+                    csGame.render();
+                });
 
-			gameState.perfFinishedJSTime = performance.now();
-			csUtils.refreshGUI(["perfStartedJSTime","perfFinishedJSTime", "perfRequireTime"]);
+                gameState.perfFinishedJSTime = performance.now();
+                csUtils.refreshGUI(["perfStartedJSTime","perfFinishedJSTime", "perfRequireTime"]);
 
-			gameState.mainLoop.start();
-		});
-	//});
-});
+                gameState.mainLoop.start();
+            });
+        //});
+    }
+);
